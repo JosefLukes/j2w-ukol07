9aab13345bbb26011bd819fe090582e6a48d35f1
JosefLukes <josef.lukes@gmail.com>
2021-05-23 09:21:21 +0200

    Přidáno mazání a editace vizitky

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
@@ -3,6 +3,7 @@ package cz.czechitas.java2webapps.ukol7.controller;
 import cz.czechitas.java2webapps.ukol7.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol7.repository.VizitkaRepository;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
-public class VizitkaController {
+public class VizitkaController<params> {
 
     private final VizitkaRepository vizitkaRepository;
 
@@ -49,10 +50,11 @@ public class VizitkaController {
 
     }
 
+
     @GetMapping(path = "/nova")
     public Object formularNovaVizitka() {
         return new ModelAndView("formular")
-                .addObject("vizitka",new Vizitka());
+                .addObject("vizitka", new Vizitka());
     }
 
     @PostMapping(path = "/nova")
@@ -65,5 +67,31 @@ public class VizitkaController {
         return "redirect:/";
     }
 
-    
+    @PostMapping(path = "/{id}", params = "akce=smazat")
+    public Object smaz(@PathVariable Integer id) {
+        vizitkaRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @PostMapping(path = "/{id}")
+    public Object edit(@PathVariable Integer id) {
+        return String.format("redirect:/edit/%d", id);
+    }
+
+    @GetMapping(path = "/edit/{id}")
+    public Object formularEditace(@PathVariable Integer id) {
+        Optional<Vizitka> vizitka = vizitkaRepository.findById(id);
+
+        return new ModelAndView("formular")
+                .addObject("vizitka", vizitka.get());
+    }
+
+    @PostMapping(path = "/edit/{id}")
+    public Object editujVizitku(@PathVariable Integer id, @ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+        vizitkaRepository.save(vizitka);
+        return String.format("redirect:/%d", vizitka.getId());
+    }
 }
